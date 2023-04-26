538d7c58b0f0a48fac6bbd4d95f96e6e8501b7b1
oh-zawa3 <oh.zawa3@gmail.com>
2023-04-26 23:01:43 +0900

    @prisma/client確認

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     UserModule,
     AuthModule,
+    PrismaModule,
   ],
 })
 export class AppModule {}
